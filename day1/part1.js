9a5f484d1c33feef5342fa208dfc041209f0b895
Oliver Schwalbe Lehtihet <oliverlehtihet@gmail.com>
2023-12-02 18:59:38 +0100

    Day 1 part 1 done

---
+++ day1/part1.js
@@ -0,0 +1,10 @@
+
+
+const fs = require("fs");
+const path = require("path");
+
+const input = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
+
+const digits = input.map(e => +(e.replace(/\D/g, "")[0] + (e.replace(/\D/g, "").slice(-1))));
+const result = digits.reduce((previous, current) => previous+current, 0);
+console.log(result);
