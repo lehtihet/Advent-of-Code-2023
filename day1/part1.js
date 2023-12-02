9ebcd9f080b98af2ee40d1a875c9d1eae52316a5
Oliver Schwalbe Lehtihet <oliverlehtihet@gmail.com>
2023-12-02 19:21:19 +0100

    Refactor part 1, workon part 2

--- day1/part1.js
+++ day1/part1.js
@@ -5,6 +5,10 @@ const path = require("path");
 
 const input = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
 
-const digits = input.map(e => +(e.replace(/\D/g, "")[0] + (e.replace(/\D/g, "").slice(-1))));
+const digits = input.map(e => {
+    const parsedString = e.replace(/\D/g, "");
+    return +(parsedString[0] + parsedString.slice(-1));
+});
+
 const result = digits.reduce((previous, current) => previous+current, 0);
 console.log(result);
--- day1/part2.js
+++ day1/part2.js
@@ -6,6 +6,10 @@ const path = require("path");
 const input = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
 
 
-const digits = input.map(e => +(e.replace(/\D/g, "")[0] + (e.replace(/\D/g, "").slice(-1))));
+const digits = input.map(e => {
+    const parsedString = (e.replace('one', '1').replace('two', '2').replace('three', '3').replace('four', '4').replace('five', '5').replace('six', '6').replace('seven', '7').replace('eight', '8').replace('nine', '9').replace(/\D/g, ""));
+    return +(parsedString[0] + parsedString.slice(-1));
+});
+
 const result = digits.reduce((previous, current) => previous+current, 0);
 console.log(result);
