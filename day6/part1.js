d5c8ed90340e5e69b8287e57464499b071586e39
Oliver Schwalbe Lehtihet <oliverlehtihet@gmail.com>
2023-12-06 10:14:57 +0100

    Refactor day 6, remove redundant speed counter

--- day6/part1.js
+++ day6/part1.js
@@ -3,20 +3,15 @@ const path = require("path");
 
 const [time, dist] = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().replace(/\r/g, '').split("\n").map(e => e.split(' ').filter(e => e != '' && Number.isInteger(+e)));
 
-let speed = 0;
 let wonRaces = 0;
 const waysToWin = [];
 
 for (let i = 0; i < time.length; i++) {
-    for (let t = 0; t < time[i]; t++) {
-        if (speed*(time[i]-t) > dist[i])
+    for (let t = 0; t < time[i]; t++) // t is both speed and time counter
+        if (t*(time[i]-t) > dist[i])
             wonRaces++;
-        speed++;
-    }
-    
     waysToWin.push(wonRaces);
     wonRaces = 0;
-    speed = 0;
 }
 
 console.log(waysToWin.reduce((a,b) => a*b, 1));
--- day6/part2.js
+++ day6/part2.js
@@ -2,13 +2,8 @@ const fs = require("fs");
 const path = require("path");
 
 const [time, dist] = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n").map(e => +(e.replace(/\s|\D/g, '')));
-let speed = 0;
 let wonRaces = 0;
-
-for (let t = 0; t < time; t++) {
-    if (speed*(time-t) > dist)
+for (let t = 0; t < time; t++)
+    if (t*(time-t) > dist)
         wonRaces++;
-    speed++;
-}
-
 console.log(wonRaces);
